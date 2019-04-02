406b01ebedaf4436e1ff06175b21c9305d5295f6
wangzh <jon-zoro@hotmail.com>
2019-04-02 16:08:00 +0800

    Initial commit

---
+++ src/test/java/com/jonz/wx/WechatServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jonz.wx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
